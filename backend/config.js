202dad5d9049402024444b893c722b758a606d45
Sourabh <Sourabhpar@gmail.com>
2024-05-20 21:58:53 +0530

    done

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 3000;
+
+export const mongoDBURL = "mongodb+srv://shabby:s9wGjZI36iTnqdhq@mongodb.ddleqqu.mongodb.net/books-collection?retryWrites=true&w=majority&appName=mongodb"
