d1fa584135b53a8bda08c1922486a2d6998a53da
Sourabh <Sourabhpar@gmail.com>
2024-05-30 07:59:39 +0530

    Added .gitignore file

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 3000;
-
-export const mongoDBURL = "mongodb+srv://shabby:s9wGjZI36iTnqdhq@mongodb.ddleqqu.mongodb.net/books-collection?retryWrites=true&w=majority&appName=mongodb"
